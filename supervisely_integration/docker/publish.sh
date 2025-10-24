cd787c1bcb864832846cebf1543f869e57ff46dd
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-10-24 15:44:14 +0400

    Update Supervisely version to v6.73.460 and Docker Image to v1.0.4 (#8)

--- supervisely_integration/docker/publish.sh
+++ supervisely_integration/docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/cotracker:1.0.1 . && \
-docker push supervisely/cotracker:1.0.1
+docker build -t supervisely/cotracker:1.0.4 . && \
+docker push supervisely/cotracker:1.0.4
