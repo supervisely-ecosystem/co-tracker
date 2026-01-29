beb19c8fbd14430036b538b8626060fda8030619
vorozhkog <vorozhkog@icloud.com>
2026-01-29 03:38:49 +0000

    upgrade sdk to v6.73.516

--- supervisely_integration/docker/publish.sh
+++ supervisely_integration/docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/cotracker:1.0.5 . && \
-docker push supervisely/cotracker:1.0.5
+docker build -t supervisely/cotracker:1.0.7 . && \
+docker push supervisely/cotracker:1.0.7
