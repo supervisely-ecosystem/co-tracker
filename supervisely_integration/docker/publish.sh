4430ce88df1343b2fccdf6cafb42bb6cd72221b6
vorozhkog <vorozhkog@icloud.com>
2026-02-10 05:06:50 +0000

    init

--- supervisely_integration/docker/publish.sh
+++ supervisely_integration/docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/cotracker:1.0.7 . && \
-docker push supervisely/cotracker:1.0.7
+docker build -t supervisely/cotracker:1.0.8 . && \
+docker push supervisely/cotracker:1.0.8
