f93225f4ce666fa1fbad1851ace290f8248dd957
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2026-02-16 21:18:42 +0000

    fix: correct docker python sdk version label; upgrade instance version

--- supervisely_integration/docker/publish.sh
+++ supervisely_integration/docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/cotracker:1.0.8 . && \
-docker push supervisely/cotracker:1.0.8
+docker build -t supervisely/cotracker:1.0.9 . && \
+docker push supervisely/cotracker:1.0.9
